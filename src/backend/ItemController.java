1d53061a9ef2f7cf0d9aea2ed1f2a63f96a2644a
Hanzeltog <me_sang@hotmail.com>
2016-07-27 16:34:27 +0200

    Added controller class for communicating between AngularJS and JAVA

---
+++ src/backend/ItemController.java
@@ -0,0 +1,21 @@
+package backend;
+
+@Controller
+@RequestMapping(value = "/item")
+public class ItemController {
+
+	private final ItemService itemService;
+	
+	@Autowired
+	public ItemController(final ItemService itemService) {
+		this.itemService = itemService;
+	}
+	
+	@RequestMapping(value = "/", method = {RequestMethod.GET})
+	@ResponseBody
+	public List<Item> getItems() {
+		return itemService.getItems();
+	}
+	
+
+}
