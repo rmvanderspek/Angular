f8c1b75a3b08a51661b3c720dd8993332d072132
rmvanderspek <rmvanderspek@gmail.com>
2016-07-27 09:48:07 +0200

    Add e-mail form

--- WebContent/controllers/add-controller.js
+++ WebContent/controllers/add-controller.js
@@ -4,5 +4,24 @@ angular.module("AMail").controller("AddCtrl", ["$scope", "$location",
 		$scope.navigate.back = function() {
 			$location.path(".//");
 		}
+		
+		$scope.action = {};
+		$scope.action.add = function() {
+			messages.push({
+				id: getNewId(), sender: $scope.sender, subject: $scope.subject,
+				date: 'Dec 7, 2013 12:32:00', recipients: [$scope.reciever],
+				message: $scope.message
+			});
+		}
+		
+		var getNewId = function() {
+			var highestId = -1;
+			for(var i = 0; i < messages.length; i++) {
+				if(messages[i].id > highestId) {
+					highestId = messages[i].id;
+				}
+			}
+			return highestId + 1;
+		}
 	}
 ]);
