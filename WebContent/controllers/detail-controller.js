8e5a920b79278bd2bff14baf4168ce19b457f4ad
rmvanderspek <rmvanderspek@gmail.com>
2016-07-29 11:26:51 +0200

    Add layout to detail list

--- WebContent/controllers/detail-controller.js
+++ WebContent/controllers/detail-controller.js
@@ -1,8 +1,16 @@
-angular.module("AMail").controller("DetailCtrl", function($scope, $routeParams) {
-	$scope.id = $routeParams.id;
-	for (var i = 0; i < items.length; i++){
-		if(items[i].id === parseInt($scope.id)){
-			$scope.item = items[i];
+angular.module("AMail").controller("DetailCtrl", ["$scope", "$location", "$routeParams", 
+    function($scope, $location, $routeParams) {
+		$scope.id = $routeParams.id;
+		for (var i = 0; i < items.length; i++){
+			if(items[i].id === parseInt($scope.id)){
+				$scope.item = items[i];
+			}
 		}
+		
+		$scope.navigate = {};
+		
+		$scope.navigate.back = function() {
+			$location.path(".//");
+		};
 	}
-});
+]);
