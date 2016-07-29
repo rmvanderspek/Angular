166b25d2b0f70d21d602f5c5ad858aa16ffeeb5a
Hanzeltog <me_sang@hotmail.com>
2016-07-29 10:49:18 +0200

    cleanup code

--- WebContent/controllers/detail-controller.js
+++ WebContent/controllers/detail-controller.js
@@ -1,14 +1,8 @@
 angular.module("AMail").controller("DetailCtrl", function($scope, $routeParams) {
 	$scope.id = $routeParams.id;
 	for (var i = 0; i < items.length; i++){
-		console.log(items.length);
-		console.log(items);
-		console.log(items[i].id);
-		console.log($scope.id);
 		if(items[i].id === parseInt($scope.id)){
 			$scope.item = items[i];
-			console.log($scope.item);
 		}
 	}
-//	$scope.item = items[$scope.id];
 });
